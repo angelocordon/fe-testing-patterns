68caedd06685c87768981c5d08f855ed58193b2a
Angelo Cordon <angelo.cordon@omadahealth.com>
2020-08-02 19:57:20 -0700

    Restructure folder to base off of Kairos codebase

---
+++ src/components/Panel.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Panel = () => (
+  <h1>Hello there</h1>
+);
+
+export default Panel;
--- src/components/Panel/Panel.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Panel = () => {
-  return (
-    <h1>Hello there</h1>
-  )
-};
-
-export default Panel;
